f112467056232f5096f31a01090b95cb1b16487a
nakanoh <murry.xxx.7@gmail.com>
2020-02-07 20:43:29 +0900

    add comment

--- redux-sample/src/redux/count/reducer.js
+++ redux-sample/src/redux/count/reducer.js
@@ -2,6 +2,7 @@ import { INCREMENT, DECREMENT } from "./constants";
 
 const initState = 0;
 
+// ~1-5で追加
 export const count = ( state=initState, {type, payload}) => {
     switch (type) {
         case INCREMENT:
